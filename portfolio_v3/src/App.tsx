c32eb2f8a216f108cbbc6ec49e8150fe1f9f933b
crvd <casparvandam@hotmail.com>
2026-02-10 19:35:21 +0100

    basic structure

--- portfolio_v3/src/App.tsx
+++ portfolio_v3/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
