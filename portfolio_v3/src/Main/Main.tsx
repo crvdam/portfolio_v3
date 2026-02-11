04b5265c89f97621be5c3907f1dd63f56d7d94e1
crvd <casparvandam@hotmail.com>
2026-02-11 21:17:09 +0100

    main introduction styling adjustments

--- portfolio_v3/src/Main/Main.tsx
+++ portfolio_v3/src/Main/Main.tsx
@@ -1,52 +1,56 @@
 import "./Main.css";
+import Socials from "../Socials/Socials";
 
 function Main() {
   return (
     <main>
       <section className="introduction tile">
-        <div>
-          <p>Hi, my name is</p>
+        <div className="introduction-wrapper">
+          <p className="superscript">Hi, my name is</p>
           <h1>Caspar van Dam</h1>
-          <h3>Web developer</h3>
+          <h3 className="subscript">Web developer</h3>
         </div>
+        <Socials />
       </section>
 
       <section className="projects">
         <h2 className="section-title">Projects</h2>
-        <div className="project tile">
-          <h3>Project name</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
-            assumenda velit vitae, libero, totam consectetur deserunt illo minus
-            maxime ex commodi sequi dicta laborum ipsa esse, distinctio error
-            animi?
-          </p>
-        </div>
-        <div className="project tile">
-          <h3>Project name</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
-            assumenda velit vitae, libero, totam consectetur deserunt illo minus
-            maxime ex commodi sequi dicta laborum ipsa esse, distinctio error
-            animi?
-          </p>
-        </div>
-        <div className="project tile">
-          <h3>Project name</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
-            assumenda velit vitae, libero, totam consectetur deserunt illo minus
-            maxime ex commodi sequi dicta laborum ipsa esse, distinctio error
-            animi?
-          </p>
-        </div>
+        <ul className="projects-list">
+          <li className="tile">
+            <h3>Project name</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
+              assumenda velit vitae, libero, totam consectetur deserunt illo
+              minus maxime ex commodi sequi dicta laborum ipsa esse, distinctio
+              error animi?
+            </p>
+          </li>
+          <li className="tile">
+            <h3>Project name</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
+              assumenda velit vitae, libero, totam consectetur deserunt illo
+              minus maxime ex commodi sequi dicta laborum ipsa esse, distinctio
+              error animi?
+            </p>
+          </li>
+          <li className="tile">
+            <h3>Project name</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
+              assumenda velit vitae, libero, totam consectetur deserunt illo
+              minus maxime ex commodi sequi dicta laborum ipsa esse, distinctio
+              error animi?
+            </p>
+          </li>
+        </ul>
       </section>
 
       <section className="resume">
-        <h2 className="section-title">Resume</h2>
+        <h2>Resume</h2>
 
         <div className="resume-subcategory">
-          <h3 className="section-title">Skills</h3>
+          <h3>Skills</h3>
           <ul className="skill-list">
             <li className="skill">TypeScript</li>
             <li className="skill">React</li>
@@ -63,7 +67,7 @@ function Main() {
         </div>
 
         <div className="resume-subcategory">
-          <h3 className="section-title">Experience</h3>
+          <h3>Experience</h3>
           <div className="experience-item tile">
             <div className="function-wrapper">
               <h4 className="function-title">Junior Front-end Developer</h4>
@@ -78,7 +82,7 @@ function Main() {
           </div>
         </div>
         <div className="resume-subcategory">
-          <h3 className="section-title">Education</h3>
+          <h3>Education</h3>
           <div className="education-item tile">
             <h4>Work & Organizational Psychology</h4>
             <p>Master's</p>
