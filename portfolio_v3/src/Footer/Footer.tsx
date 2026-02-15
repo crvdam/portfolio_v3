ae9a18f91cd88bfd4eb5b99f3e30c933a382faea
crvd <casparvandam@hotmail.com>
2026-02-15 17:04:06 +0100

    added footer

--- portfolio_v3/src/Footer/Footer.tsx
+++ portfolio_v3/src/Footer/Footer.tsx
@@ -1,7 +1,15 @@
+import Socials from "../Socials/Socials";
 import "./Footer.css";
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <Socials />
+      <div className="text-tiny">
+        © <span>{new Date().getFullYear()}</span> - Caspar van Dam
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
